d5b823d1396a1965f1e251581259f4d61d8780ee
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-15 22:15:09 +0100

    Add deleteById endpoint to TipsRouter

--- src/server/router/tipsRouters/tipsRouters.ts
+++ src/server/router/tipsRouters/tipsRouters.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  deleteTipById,
   getMyTips,
   getTips,
 } from "../../controllers/tipsControllers/tipsControllers.js";
@@ -9,3 +10,4 @@ export const tipsRouter = Router();
 
 tipsRouter.get(endpoints.root, getTips);
 tipsRouter.get(endpoints.myTips, getMyTips);
+tipsRouter.delete(`${endpoints.delete}${endpoints.id}`, deleteTipById);
